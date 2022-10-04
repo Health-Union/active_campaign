58e96336b3f0153f8cea35ddf86c84d240f554c7
Steven DeGele <arghmeleg@users.noreply.github.com>
2022-10-03 21:21:08 -0400

    status module

---
+++ lib/active_campaign/contact/status.ex
@@ -0,0 +1,25 @@
+defmodule ActiveCampaign.Contact.Status do
+  @moduledoc """
+  A contact's status is represented by an integer constant.
+  """
+  @any -1
+  @unconfirmed 0
+  @active 1
+  @unsubscribed 2
+  @bounced 3
+
+  @spec any :: integer()
+  def any, do: @any
+
+  @spec unconfirmed :: integer()
+  def unconfirmed, do: @unconfirmed
+
+  @spec active :: integer()
+  def active, do: @active
+
+  @spec unsubscribed :: integer()
+  def unsubscribed, do: @unsubscribed
+
+  @spec bounced :: integer()
+  def bounced, do: @bounced
+end
