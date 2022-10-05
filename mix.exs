694c0dc5d918dca1b6a179d09fe8308a696c9ba9
Steven DeGele <arghmeleg@users.noreply.github.com>
2022-10-04 20:34:21 -0400

    add license

--- mix.exs
+++ mix.exs
@@ -1,10 +1,13 @@
 defmodule ActiveCampaign.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/Health-Union/active_campaign"
+  @version "0.1.0"
+
   def project do
     [
       app: :active_campaign,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,6 +24,16 @@ defmodule ActiveCampaign.MixProject do
     [
       {:httpoison, "~> 1.8"},
       {:jason, "~> 1.4"}
+    ]
+  end
+
+  defp package do
+    [
+      description: "A basic Elixir wrapper for version 3 of the ActiveCampaign API.",
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Steve DeGele"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
